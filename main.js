c69d1aebb6a98885bcdef2f84e139a8f3eb989e6
Fathandi <168356153+Fathandi@users.noreply.github.com>
2024-05-24 21:57:03 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+onload = () =>{
+        document.body.classList.remove("container");
+};
+
+setTimeout(function() {
+        window.location.href = "index.html";
+    }, 10000); // 3500 milidetik = 3,5 detik
