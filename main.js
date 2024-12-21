638214f9e2851fc2341921598ec88ef919650b7d
Fathandi <168356153+Fathandi@users.noreply.github.com>
2024-12-21 13:41:37 +0700

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-onload = () =>{
-        document.body.classList.remove("container");
-};
-
-setTimeout(function() {
-        window.location.href = "index.html";
-    }, 10000); // 3500 milidetik = 3,5 detik
