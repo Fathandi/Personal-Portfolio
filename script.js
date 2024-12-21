6257ba923890e4020e35e8a34199efa3c4ae9a7a
Fathandi <168356153+Fathandi@users.noreply.github.com>
2024-12-21 13:59:19 +0700

    Update script.js

--- script.js
+++ script.js
@@ -9,60 +9,34 @@ function myMenuFunction() {
   }
 }
 
-
 /* ----- BLUE-BTN FUNCTION ----- */
 function redirectToAssistant() {
-  window.open('./assistant/assistant.html', '_blank');
+  window.open("./assistant/assistant.html", "_blank");
 }
 
 /* ----- DOWNLOAD CV FUNCION ----- */
-function downloadCV(){
-  Swal.fire({
-    imageUrl: "persentSkill.jpg",
-    confirmButtonText: "Auto Diagram By Fathandi"
-  }).then((result) => {
-    if (result.isConfirmed) {
-      window.location.href = 'https://fathandi.github.io/Auto-Diagram/';
-    }
-  });
-
-    // // Ganti 'path/to/cv.pdf' dengan URL atau path file CV Anda
-    // const cvFileUrl = '';
-
-    // // Buat elemen anchor untuk trigger unduhan
-    // const downloadLink = document.createElement('a');
-    // downloadLink.href = cvFileUrl;
-    // downloadLink.download = 'Fathih-Apriandi-CV.jpg'; // Ganti dengan nama file CV Anda
-  
-    // // Tambahkan elemen anchor ke dokumen dan klik
-    // document.body.appendChild(downloadLink);
-    // downloadLink.click();
-  
-    // // Hapus elemen anchor setelah unduhan     
-    // document.body.removeChild(downloadLink);
-  }
+function downloadCV() {
+  window.open(".cv/tempCV.txt", "_blank");
+}
 
 /* ----- ADD SHADOW ON NAVIGATION BAR WHILE SCROLLING ----- */
-window.onscroll = function () { headerShadow() };
+window.onscroll = function () {
+  headerShadow();
+};
 
 function headerShadow() {
   const navHeader = document.getElementById("header");
 
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-
     navHeader.style.boxShadow = "0 1px 6px rgba(0, 0, 0, 0.1)";
     navHeader.style.height = "70px";
     navHeader.style.lineHeight = "70px";
-
   } else {
-
     navHeader.style.boxShadow = "none";
     navHeader.style.height = "90px";
     navHeader.style.lineHeight = "90px";
-
   }
 }
-
 
 /* ----- TYPING EFFECT ----- */
 let typingEffect = new Typed(".typedText", {
@@ -70,135 +44,134 @@ let typingEffect = new Typed(".typedText", {
   loop: true,
   typeSpeed: 100,
   backSpeed: 80,
-  backDelay: 2000
-})
-
+  backDelay: 2000,
+});
 
 /* ----- ## -- SCROLL REVEAL ANIMATION -- ## ----- */
 const sr = ScrollReveal({
-  origin: 'top',
-  distance: '80px',
+  origin: "top",
+  distance: "80px",
   duration: 2000,
-  reset: true
-})
+  reset: true,
+});
 
 /* -- HOME -- */
-sr.reveal('.featured-text-card', {})
-sr.reveal('.featured-name', { delay: 100 })
-sr.reveal('.containerload', { delay: 200 })
-sr.reveal('.featured-text-btn', { delay: 200 })
-sr.reveal('.social_icons', { delay: 200 })
-sr.reveal('.featured-image', { delay: 300 })
-
+sr.reveal(".featured-text-card", {});
+sr.reveal(".featured-name", { delay: 100 });
+sr.reveal(".containerload", { delay: 200 });
+sr.reveal(".featured-text-btn", { delay: 200 });
+sr.reveal(".social_icons", { delay: 200 });
+sr.reveal(".featured-image", { delay: 300 });
 
 /* -- PROJECT BOX -- */
-sr.reveal('.project-box', { interval: 200 })
+sr.reveal(".project-box", { interval: 200 });
 
 /* -- HEADINGS -- */
-sr.reveal('.top-header', {})
+sr.reveal(".top-header", {});
 
 /* ----- ## -- SCROLL REVEAL LEFT_RIGHT ANIMATION -- ## ----- */
 
 /* -- ABOUT INFO & CONTACT INFO -- */
 const srLeft = ScrollReveal({
-  origin: 'left',
-  distance: '80px',
+  origin: "left",
+  distance: "80px",
   duration: 2000,
-  reset: true
-})
+  reset: true,
+});
 
-srLeft.reveal('.about-info', { delay: 100 })
-srLeft.reveal('.contact-info', { delay: 100 })
+srLeft.reveal(".about-info", { delay: 100 });
+srLeft.reveal(".contact-info", { delay: 100 });
 
 /* -- ABOUT SKILLS & FORM BOX -- */
 const srRight = ScrollReveal({
-  origin: 'right',
-  distance: '80px',
+  origin: "right",
+  distance: "80px",
   duration: 2000,
-  reset: true
-})
+  reset: true,
+});
 
-srRight.reveal('.skills-box', { delay: 100 })
-srRight.reveal('.form-control', { delay: 100 })
-
-
+srRight.reveal(".skills-box", { delay: 100 });
+srRight.reveal(".form-control", { delay: 100 });
 
 /* ----- CHANGE ACTIVE LINK ----- */
 
-const sections = document.querySelectorAll('section[id]')
+const sections = document.querySelectorAll("section[id]");
 
 function scrollActive() {
   const scrollY = window.scrollY;
 
-  sections.forEach(current => {
+  sections.forEach((current) => {
     const sectionHeight = current.offsetHeight,
       sectionTop = current.offsetTop - 50,
-      sectionId = current.getAttribute('id')
+      sectionId = current.getAttribute("id");
 
     if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-
-      document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link')
-
+      document
+        .querySelector(".nav-menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
     } else {
-
-      document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link')
-
+      document
+        .querySelector(".nav-menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
     }
-  })
+  });
 }
-
 
 /* ----- CONTACT BOX EMAIL JS -----*/
 
-document.addEventListener('DOMContentLoaded', function () {
-  emailjs.init('7K2aOKMaqg5DT64wQ'); // Menggunakan Public ID yang baru
+document.addEventListener("DOMContentLoaded", function () {
+  emailjs.init("7K2aOKMaqg5DT64wQ"); // Menggunakan Public ID yang baru
 
-  document.getElementById('sendBtn').addEventListener('click', function (event) {
-    event.preventDefault();
+  document
+    .getElementById("sendBtn")
+    .addEventListener("click", function (event) {
+      event.preventDefault();
 
-    const serviceID = 'service_9vagnoe'; // Menggunakan Service ID yang baru
-    const templateID = 'template_zq7n4mh'; // Menggunakan Template ID yang baru
+      const serviceID = "service_9vagnoe"; // Menggunakan Service ID yang baru
+      const templateID = "template_zq7n4mh"; // Menggunakan Template ID yang baru
 
-    // Ambil nilai dari input nama, email, dan pesan
-    const senderName = document.getElementById('name').value;
-    const senderEmail = document.getElementById('email').value;
-    const message = document.getElementById('message').value;
+      // Ambil nilai dari input nama, email, dan pesan
+      const senderName = document.getElementById("name").value;
+      const senderEmail = document.getElementById("email").value;
+      const message = document.getElementById("message").value;
 
-    // Pastikan input nama, email, dan pesan tidak kosong sebelum mengirim
-    if (!senderName || !senderEmail || !message) {
-      Swal.fire({
-        title: "Please fill in all fields!",
-        timer: 2000,
-        showConfirmButton: false
-      });
-      return;
-    }
-
-    const templateParams = {
-      from_name: senderName,
-      from_email: senderEmail,
-      message_html: message
-    };
-
-    emailjs.send(serviceID, templateID, templateParams)
-      .then(response => {
-        console.log('SUCCESS!', response.status, response.text);
+      // Pastikan input nama, email, dan pesan tidak kosong sebelum mengirim
+      if (!senderName || !senderEmail || !message) {
         Swal.fire({
-          position: "center",
-          icon: "success",
-          title: "Your message has been sent successfully!",
+          title: "Please fill in all fields!",
+          timer: 2000,
           showConfirmButton: false,
-          timer: 1500
         });
-      }, error => {
-        console.log('FAILED...', error);
-        Swal.fire({
-          icon: "error",
-          title: "Oops...",
-          text: "Failed to send your message. Please try again later",
-        });
-      });
-  });
+        return;
+      }
+
+      const templateParams = {
+        from_name: senderName,
+        from_email: senderEmail,
+        message_html: message,
+      };
+
+      emailjs.send(serviceID, templateID, templateParams).then(
+        (response) => {
+          console.log("SUCCESS!", response.status, response.text);
+          Swal.fire({
+            position: "center",
+            icon: "success",
+            title: "Your message has been sent successfully!",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        },
+        (error) => {
+          console.log("FAILED...", error);
+          Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Failed to send your message. Please try again later",
+          });
+        }
+      );
+    });
 });
 
-window.addEventListener('scroll', scrollActive)
+window.addEventListener("scroll", scrollActive);
